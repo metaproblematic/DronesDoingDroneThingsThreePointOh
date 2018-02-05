57c08f83774d3a2cdd710ed524974443477ddfbe
Travis Valenti <ennuuos@gmail.com>
2018-02-05 15:42:34 +1100

    Convert tests to use ES6 classes, add more tests
    I converted the tests to work with the ES6 class structure I added in the previous commit (3871b42a616ffb079554a95d1f98955077f05746).
    
    Also added are tests for the constructor, testing with and without arbitrary options passed as parameters.

--- test/drones.js
+++ test/drones.js
@@ -1,22 +1,43 @@
 
+var Drones = new require("../drones");
 var expect = require("chai").expect;
-var Drones = require("../drones");
 
 
-
-describe("Drone management and utility module", function() {
-    describe("Retrieve all drone objects", function() {
+describe("Drones.js - Drone management and utility module", function() {
+    describe("Drones Constructor", function() {
+        it("initializes correctly with no options", function() {
+            let drones = new Drones();
+            expect(drones._all).to.deep.equal([]);
+            expect(drones._options).to.deep.equal({});
+        });
+        it("initializes correctly with options", function() {
+            let drones = new Drones({
+                port: 3000,
+                parameter: "param",
+            });
+            expect(drones._all).to.deep.equal([]);
+            expect(drones._options).to.deep.equal({
+                port: 3000,
+                parameter: "param",
+            });
+        });
+    });
+    describe("Drones.all", function() {
         it("returns no drone objects", function() {
-            let all = Drones.all();
+            let drones = new Drones();
+
+            let all = drones.all();
             expect(all).to.deep.equal([]);
         });
         it("returns the correct amount of drone objects", function() {
-            Drones._add(1);
-            let all = Drones.all();
+            let drones = new Drones();
+
+            drones._add(1);
+            let all = drones.all();
             expect(all).to.have.lengthOf(1);
 
-            Drones._add(2);
-            all = Drones.all();
+            drones._add(2);
+            all = drones.all();
             expect(all).to.have.lengthOf(2);
         });
     });
