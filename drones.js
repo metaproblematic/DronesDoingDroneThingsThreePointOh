3871b42a616ffb079554a95d1f98955077f05746
Travis Valenti <ennuuos@gmail.com>
2018-02-05 15:40:23 +1100

    Convert Drones object to ES6 class structure
    Doing this simplifies the code base and makes it more readable, as well as abstracting the implementation of classes from the code itself.
    
    Writing classes this way makes it easier for developers used to more "traditional"* object oriented programming to work with this class.

--- drones.js
+++ drones.js
@@ -1,129 +1,128 @@
-/**
- * This module contains all information and functions relevant to drones.
- * It is designed to work independantly from the REST API.
- * @module
- * @requires ar-drone {@link https://www.npmjs.com/package/ar-drone}
- */
-
 const ar_module = require('ar-drone');
 const ping = require('ping');
 
 const config = require('./config.json');
 
-module.exports = new Drones;
-
 /**
  * Represents all drones and functions relevant to drones
- * @constructor
- * @param {Object} options - Set of configuration options. (See parseOptions)
- * @todo Test this constructor
+ * @class
  */
-function Drones(options) {
-    this._all = [];
-    this._options = options;
+class Drones {
+
+
+    /** Constructor for Drones class
+     * @constructor
+     * @param {Object} options - Set of configuration options. (See parseOptions)
+     */
+    constructor(options) {
+        this._all = [];
+        this._options = options || {};
+    }
+
+    /**
+     * Returns an array of all drone objects, irregardless of if they are confirmed.
+     * @returns {Drone[]} Array containing all drone objects.
+     */
+    all() {
+        return this._all;
+    }
+
+    /**
+     * Returns an array of all drone objects that are confirmed to be active.
+     * @returns {Drone[]} Array containing all confirmed drone objects.
+     * @todo Test this function
+     */
+    allConfirmed() {
+        let allConfirmed = this._all;
+        return this._all;
+    }
+
+    /**
+     * Adds a new drone object to the list with the given id.
+     * @param {int} id - Id of drone to create.
+     * @returns {Drone} Newly created client object from ar-drone module or pre-existing object with same id.
+     * @todo Test this function
+     */
+    _add(id) {
+        if(this._containsId(id)) return this.getDrone(id);
+        let drone = ar_module.createClient({
+            ip : this._ipTemplate(id) // e.g. 192.168.1.101
+        });
+        this._all.push(drone);
+        drone.resume(); // Let's hope this fixes an issue with drones not responding on second connect
+        drone.on('navdata', data => {drone.navdata = data; console.log(data)});
+        drone.animateLeds('blinkOrange', 5, 1); // This animation lets us know the drone has connected
+        if(this._options.log) console.log(`Drone ${id} connected`);
+        return drone;
+    }
+
+    /**
+     * @todo Write the documentation.
+     * @todo Test this function.
+     */
+    _remove(id) {
+        if(!this._containsId(id)) return false;
+        delete this.getDrone(id);
+    }
+
+    /**
+     * @param {int} id - Id of drone to search for.
+     * @returns {boolean} Weather a drone with id exists.
+     * @todo Write the documentation.
+     * @todo Test this function.
+     */
+    _containsId(id) {
+        return this._all
+            .map(drone => drone.id)
+            .filter(_id => _id === id)
+            .length > 0;
+    }
+
+    /**
+     * @param {int} id - Id of drone to return.
+     * @returns {Drone}
+     * @todo Write the documentation.
+     * @todo Test this function.
+     */
+    getDrone(id) {
+        return this._all
+            .map(drone => drone.id)
+            .filter(_id => _id === id)
+            [0];
+    }
+
+    /**
+     * @todo Write the documentation.
+     * @todo Test this function.
+     */
+    _ipTemplate(id) {
+        return `${config.network.drone_ip_stub}${id}`;
+    }
+
+    /**
+     * @todo Write the documentation.
+     * @todo Test this function.
+     */
+    _pingAll() {
+        config.network.drone_id_list.forEach(id => this._ping(id));
+    }
+
+     /**
+      * @todo Write the documentation.
+      * @todo Test this function.
+      */
+    _ping(id) {
+        ping.sys.probe(
+            this._ipTemplate(id),
+            success => {
+                if(success) create(id);
+                if(!success) remove(id);
+                (success ? this._add : this._remove)(id)
+            },
+            {'timeout': 1}
+        );
+    }
+
 }
 
-/**
- * Returns an array of all drone objects, irregardless of if they are confirmed.
- * @returns {Drone[]} Array containing all drone objects.
- * @todo Test this function
- */
-Drones.prototype.all = function() {
-    return this._all;
-}
-
-/**
- * Returns an array of all drone objects that are confirmed to be active.
- * @returns {Drone[]} Array containing all confirmed drone objects.
- * @todo Test this function
- */
-Drones.prototype.allConfirmed = function() {
-    let allConfirmed = this._all;
-    return this._all;
-}
-
-/**
- * Adds a new drone object to the list with the given id.
- * @param {int} id - Id of drone to create.
- * @returns {Drone} Newly created client object from ar-drone module or pre-existing object with same id.
- * @todo Test this function
- */
-Drones.prototype._add = function(id) {
-    if(this._containsId(id)) return this.getDrone(id);
-    let drone = ar_module.createClient({
-        ip : this._ipTemplate(id) // e.g. 192.168.1.101
-    });
-    this._all.push(drone);
-    drone.resume(); // Let's hope this fixes an issue with drones not responding on second connect
-    drone.on('navdata', data => {drone.navdata = data; console.log(data)});
-    drone.animateLeds('blinkOrange', 5, 1); // This animation lets us know the drone has connected
-    console.log(`Drone ${id} connected`);
-    return drone;
-}
-
-/**
- * @todo Write the documentation.
- * @todo Test this function.
- */
-Drones.prototype._remove = function(id) {
-    if(!this._containsId(id)) return false;
-    delete this.getDrone(id);
-}
-
-/**
- * @param {int} id - Id of drone to search for.
- * @returns {boolean} Weather a drone with id exists.
- * @todo Write the documentation.
- * @todo Test this function.
- */
-Drones.prototype._containsId = function(id) {
-    return this._all
-        .map(drone => drone.id)
-        .filter(_id => _id === id)
-        .length > 0;
-}
-
-/**
- * @param {int} id - Id of drone to return.
- * @returns {Drone}
- * @todo Write the documentation.
- * @todo Test this function.
- */
-Drones.prototype.getDrone = function(id) {
-    return this._all
-        .map(drone => drone.id)
-        .filter(_id => _id === id)
-        [0];
-}
-
-/**
- * @todo Write the documentation.
- * @todo Test this function.
- */
-Drones.prototype._ipTemplate = function(id) {
-    return `${config.network.drone_ip_stub}${id}`;
-}
-
-/**
- * @todo Write the documentation.
- * @todo Test this function.
- */
-Drones.prototype._pingAll = function() {
-    config.network.drone_id_list.forEach(id => this._ping(id));
-}
-
- /**
-  * @todo Write the documentation.
-  * @todo Test this function.
-  */
-Drones.prototype._ping = function(id) {
-    ping.sys.probe(
-        this._ipTemplate(id),
-        success => {
-            if(success) create(id);
-            if(!success) remove(id);
-            (success ? this._add : this._remove)(id)
-        },
-        {'timeout': 1}
-    );
-}
+module.exports = Drones;
